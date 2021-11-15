cc0e434deca03c05ee5c1ca1b8e57e6b7abc145a
pmarkgraf-bionaut <philip.markgraf@bionautlabs.com>
2021-11-15 15:42:52 -0800

    r Convert fizz to guarded function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 pub fn fizzbuzz(n: u32) -> String {
-    if n % 15 == 0 { return "fizzbuzz".to_string(); }
-    if n % 3 == 0 { return "fizz".to_string(); }
-    if n % 5 == 0 { return "buzz".to_string(); }
-    n.to_string()
+    let mut result = String::new();
+    result.push_str(&divisible_by_three_is_fizz(n));
+    if n % 5 == 0 { result.push_str( "buzz"); }
+    if result.len() == 0 { result = n.to_string(); }
+    result
+}
+
+fn divisible_by_three_is_fizz(n: u32) -> String {
+    if n % 3 != 0 { return "".to_string(); }
+    "fizz".to_string()
 }
