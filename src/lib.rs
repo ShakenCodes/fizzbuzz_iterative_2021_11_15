ad977e1ea12a7991e6b5c16922ba68c7766dfe57
pmarkgraf-bionaut <philip.markgraf@bionautlabs.com>
2021-11-15 15:48:46 -0800

    r Convert not fizz or buzz to guarded function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 pub fn fizzbuzz(n: u32) -> String {
     let mut result = divisible_by_three_is_fizz(n);
     result.push_str( &divisible_by_five_is_buzz(n));
-    if result.len() == 0 { result = n.to_string(); }
-    result
+    not_fizz_or_buzz_is_number(result, n)
 }
 
 fn divisible_by_three_is_fizz(n: u32) -> String {
@@ -13,4 +12,9 @@ fn divisible_by_three_is_fizz(n: u32) -> String {
 fn divisible_by_five_is_buzz(n: u32) -> String {
     if n % 5 != 0 { return "".to_string(); }
     "buzz".to_string()
+}
+
+fn not_fizz_or_buzz_is_number(s: String, n: u32) -> String {
+    if s.len() > 0 { return s; }
+    n.to_string()
 }
