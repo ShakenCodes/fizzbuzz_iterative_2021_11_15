c39ca3ddf2cfbfa0da7ff8d9f07b4e51b5cc7d67
pmarkgraf-bionaut <philip.markgraf@bionautlabs.com>
2021-11-15 14:50:59 -0800

    r Revert refactorign to guard

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 pub fn fizzbuzz(n: u32) -> String {
-    if n % 15 != 0 { 
-        if n % 3 == 0 { return "fizz".to_string(); }
-        if n % 5 == 0 { return "buzz".to_string(); }
-        return n.to_string();
-    }
-    "fizzbuzz".to_string()
+    if n % 15 == 0 { return "fizzbuzz".to_string(); }
+    if n % 3 == 0 { return "fizz".to_string(); }
+    if n % 5 == 0 { return "buzz".to_string(); }
+    n.to_string()
 }
