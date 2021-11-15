5111fb18f0d49ef0b8f8e184d9a7600f0641323a
pmarkgraf-bionaut <philip.markgraf@bionautlabs.com>
2021-11-15 15:54:41 -0800

    r Name improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 pub fn fizzbuzz(n: u32) -> String {
-    let text_result = divisible_by_three_is_fizz(n) + &divisible_by_five_is_buzz(n);
-    not_fizz_or_buzz_is_number(text_result, n)
+    let text = div_by_three_is_fizz(n) + &div_by_five_is_buzz(n);
+    not_fizz_nor_buzz_is_number(text, n)
 }
 
-fn divisible_by_three_is_fizz(n: u32) -> String {
+fn div_by_three_is_fizz(n: u32) -> String {
     if n % 3 != 0 { return "".to_string(); }
     "fizz".to_string()
 }
 
-fn divisible_by_five_is_buzz(n: u32) -> String {
+fn div_by_five_is_buzz(n: u32) -> String {
     if n % 5 != 0 { return "".to_string(); }
     "buzz".to_string()
 }
 
-fn not_fizz_or_buzz_is_number(s: String, n: u32) -> String {
+fn not_fizz_nor_buzz_is_number(s: String, n: u32) -> String {
     if s.len() > 0 { return s; }
     n.to_string()
 }
